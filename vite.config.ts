9b7c3ef7e8c8a2cd0fe093bbe200708129ec890c
indikarathnindawork-cyber <indikarathnindawork@gmail.com>
2026-07-21 11:59:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-     base: '/maths_with_indika/', 
+     base: '/indikarathninda.lk/', 
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
